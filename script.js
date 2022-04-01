4d6f4e1c6d69e7e53b7ed558d192fb6fa21c4c9d
Serge Ntumba <s7ntumba@gmail.com>
2022-04-01 16:58:50 +0100

    make blue default theme

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ for (var i = 0; themeDots.length > i; i++) {
 }
 
 function setTheme(mode) {
-  if (mode == "light") {
+  if (mode == "blue") {
     document.getElementById("theme-style").href = "default.css";
   }
 
-  if (mode == "blue") {
+  if (mode == "light") {
     document.getElementById("theme-style").href = "blue.css";
   }
 
